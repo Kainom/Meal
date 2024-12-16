69cea7c85acfa35034cd532ff0d029bbae30cbe3
Kainom <eleuteriokaina@gmail.com>
2024-12-16 20:57:05 -0300

    Handle with errors

---
+++ src/app/error.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Error():JSX.Element{
+    return(
+        <h1>Error Page</h1>
+    )
+
+}
---
+++ src/app/meals/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error(): JSX.Element {
+  return (
+    <>
+      <h1>Error Page</h1>
+      <p>Fail in get meals</p>
+    </>
+  );
+}
