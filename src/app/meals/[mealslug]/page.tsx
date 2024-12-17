03ab6384b3c5b2669690e5f9214bcd46c5ae20d6
Kainom <eleuteriokaina@gmail.com>
2024-12-17 14:52:04 -0300

    I added the notFound page and mealslug dynamic page.They aren't responsive,Maybe I'll get to it later

--- src/app/meals/[mealslug]/page.tsx
+++ src/app/meals/[mealslug]/page.tsx
@@ -1,10 +1,51 @@
- function Meal(){
-    return(
-        <main>
-            <h1>Meal</h1>
-        </main>
-    )
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "../../../../lib/meals";
+import { Params } from "next/dist/server/request/params";
+
+interface Meals {
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
 }
 
+async function Meal({ params }: { params: Params }): Promise<JSX.Element> {
+  const { mealslug }: Params = await params;
+  const meal: Meals = getMeal(mealslug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image
+            src={`${meal.image}`}
+            alt={meal.title}
+            fill
+          ></Image>
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={`${classes.creator} my-3`}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary} </p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
 
 export default Meal;
